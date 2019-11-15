b01fa52cacfed5bce6fe73548478389b45973139
Wangjingning <55074713+18214887435@users.noreply.github.com>
2019-11-15 15:55:45 +0800

    BLOG

---
+++ src/main/java/com/wjn/privateblog/PrivateblogApplication.java
@@ -0,0 +1,16 @@
+package com.wjn.privateblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class PrivateblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wjn/privateblog/PrivateblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjn.privateblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
